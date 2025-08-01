8471e43e80c2c64bccd9a0d8ce105e4d2eff82df
alegian <alegiannx@gmail.com>
2025-08-01 08:45:12 +0300

    surround & telescope

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # nvim
 alegian's nvim configuration
 ## Requirements
-- `lua` and `luarocks`
-- a clipboard tool like `xclip`
+- `lua` and `luarocks` for `lazy.nvim`
+- a clipboard tool like `xclip` for yanking to system buffer
+- `ripgrep` for `telescope`
--- init.lua
+++ init.lua
@@ -1,5 +1,11 @@
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.incsearch = true
 vim.opt.ignorecase = true
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 require("config.lazy")
