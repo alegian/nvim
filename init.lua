7b409c511da0be59b9a944718541aa2af5d7194c
alegian <alegiannx@gmail.com>
2025-08-03 10:12:30 +0300

    better settings

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 vim.opt.clipboard:append("unnamedplus")
 vim.opt.incsearch = true
+vim.opt.hlsearch = false
 vim.opt.ignorecase = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
@@ -10,10 +11,21 @@ vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, { noremap = true, silent = true })
-vim.keymap.set("n", "gr", vim.lsp.buf.references, { noremap = true, silent = true })
-vim.keymap.set("n", "rn", vim.lsp.buf.rename, { noremap = true })
-vim.keymap.set("n", "H", vim.lsp.buf.hover, { noremap = true, silent = true })
-vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { noremap = true, silent = true })
+vim.opt.termguicolors = true
+vim.opt.scrolloff = 8
+vim.opt.colorcolumn = "80"
+vim.opt.updatetime = 50
+vim.opt.signcolumn = "yes"
+
+vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { silent = true })
+vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { silent = true })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { silent = true })
+vim.keymap.set("n", "H", vim.lsp.buf.hover, { silent = true })
+vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { silent = true })
+vim.keymap.set("x", "<leader>p", '"_dP', { silent = true })
+vim.keymap.set("n", "<leader>d", '"_d', { silent = true })
+vim.keymap.set("x", "<leader>d", '"_d', { silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
 
 require("config.lazy")
