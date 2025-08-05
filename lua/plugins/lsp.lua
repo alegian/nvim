ec8eb1fbca4af9a5af6f307cba9622b41a294c0b
alegian <alegiannx@gmail.com>
2025-08-05 19:28:11 +0300

    this at least boots

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -36,15 +36,15 @@ return {
   },
   {
     "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
+    config = function()
+      local lint = require("lint")
+      lint.linters_by_ft = {
         javascript = { "eslint_d" },
         typescript = { "eslint_d" },
         javascriptreact = { "eslint_d" },
         typescriptreact = { "eslint_d" },
-      },
-    },
-    config = function()
+      }
+
       vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
         callback = function()
           require("lint").try_lint()
