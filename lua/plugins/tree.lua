435fd36b2f7a69eb458f73c004b3038dc8653b49
alegian <alegiannx@gmail.com>
2025-08-28 13:05:32 +0300

    navigate up the tree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -25,6 +25,10 @@ return {
               use_float = false,
             },
           },
+          ["<bs>"] = function(state)
+            local node = state.tree:get_node()
+            require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+          end,
         },
       },
       follow_current_file = {
